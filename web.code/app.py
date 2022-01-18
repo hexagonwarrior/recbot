5f0a82f8bcc642fb74d80b2a793a79f3a1f523e0
ZhangTao <zhangtao@gnnet.com.cn>
2022-01-18 23:55:02 +0800

    fix wordcloud

--- web.code/app.py
+++ web.code/app.py
@@ -29,15 +29,14 @@ rl.init_rec()
 
 
 # 这里对数据库内容进行提取
-spider_business.run()
-spider_entertainment.run()
-spider_game.run()
-spider_military.run()
-spider_politics.run()
-spider_society.run()
-spider_sports.run()
-spider_technology.run()
-
+# spider_business.run()
+# spider_entertainment.run()
+# spider_game.run()
+# spider_military.run()
+# spider_politics.run()
+# spider_society.run()
+# spider_sports.run()
+# spider_technology.run()
 
 datalist = useful_functions.get_rec_datalist()
 
--- web.code/word_cloud.py
+++ web.code/word_cloud.py
@@ -11,6 +11,10 @@ import jieba.analyse
 
 #准备词云所需的文字（词）
 
+# stopwords = {}.fromkeys(['的', '了', '将'])
+stopwords = {}.fromkeys([k.strip() for k in open('stopwords.list', encoding="utf-8").readlines() if k.strip() != ''])
+
+
 datalist = []
 cnn = pymysql.connect(host='127.0.0.1', user='rec', password='shujuku', port=3306, database='news_with_keyword',
                       charset='utf8')
@@ -23,18 +27,25 @@ cursor.close()
 cnn.close()
 
 text = " "
-# 取出数据库中的新闻内容，进行分词
+# 取出数据库中的新闻标题，进行分词
 for item in datalist:
-    text =  text + item[4]
+    text =  text + item[1]
 cut = jieba.cut(text)
-string = ' '.join(cut)
 
-img = Image.open(r'.\static\assets\img\tree.jpg')   #打开遮罩图片
+# 去停用词
+string = ""
+for item in cut:
+    if item not in stopwords:
+        string += item + " "
+
+img = Image.open(r'static/assets/img/tree.jpg')   #打开遮罩图片
 img_array = np.array(img)   #将图片转换为数组
 wc = WordCloud(
     background_color='white',
     mask=img_array,
-    font_path="msyh.ttc"    #字体所在位置：C:\Windows\Fonts
+    font_path="wqy-microhei.ttc",
+    width="400",
+    height=400
 )
 wc.generate_from_text(string)
 
@@ -43,24 +54,7 @@ fig = plt.figure(1)
 plt.imshow(wc)
 plt.axis('off')     #是否显示坐标轴
 
-plt.show()    #显示生成的词云图片
+# plt.show()    #显示生成的词云图片
 
 #输出词云图片到文件
-plt.savefig(r'.\static\assets\img\key_word.jpg',dpi=500)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+plt.savefig(r'static/assets/img/key_word.jpg',dpi=500)
